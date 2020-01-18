9f00b61c8e1b16849b18a17de6bffc59d3778e27
Chriistelle <m.chriistelle1999@hotmail.fr>
2020-01-18 13:12:50 +0100

    module Accueil fait

--- srcs/module/accueil/vue_accueil.php
+++ srcs/module/accueil/vue_accueil.php
@@ -42,7 +42,7 @@ require_once  __dir__ .'/../../include/vue_generique.php';
 					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.781281794208!2d2.462219315674514!3d48.86238097928778!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e612ae6a556ee1%3A0x6aa2df8d4d5f21d9!2s140%20Rue%20de%20la%20Nouvelle%20France%2C%2093100%20Montreuil!5e0!3m2!1sfr!2sfr!4v1574613662165!5m2!1sfr!2sfr" width="700" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 				</div>
 				<div id="infos">
-					<p><strong>IUT de Montreuil</strong></p>
+					<p><strong> IUT de Montreuil </strong></p>
 					<p>140 rue de la nouvelle France</p>
 					<p>93100 Montreuil</p>
 					<p><a href="https://www.iut.univ-paris8.fr/">Site web</a></p>
